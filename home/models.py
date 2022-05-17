57a2c77d885d7d2aa48f3c843a909dfd25f7d22f
Daniel Cienkosz <danielcienkosz1@interia.pl>
2022-05-17 20:25:50 +0200

    connect postgresql

--- home/models.py
+++ home/models.py
@@ -4,9 +4,9 @@ from django.urls import reverse
 
 # Create your models here.
 class MatchResult(models.Model):
-    id_match = models.IntegerField(primary_key=True)
-    isResult = models.CharField(max_length=7)
-    date = models.DateTimeField()
+    id = models.IntegerField(primary_key=True)
+    isresult = models.CharField(max_length=7)
+    datetime = models.DateTimeField()
     season = models.IntegerField()
     league = models.CharField(max_length=20)
     h_id = models.IntegerField()
@@ -17,46 +17,46 @@ class MatchResult(models.Model):
     a_short_title = models.CharField(max_length=10)
     goals_h = models.IntegerField(null=True)
     goals_a = models.IntegerField(null=True)
-    xG_h = models.DecimalField(max_digits=7, decimal_places=5, blank=True)
-    xG_a = models.DecimalField(max_digits=7, decimal_places=5, blank=True)
+    xg_h = models.DecimalField(max_digits=7, decimal_places=5, null=True)
+    xg_a = models.DecimalField(max_digits=7, decimal_places=5, null=True)
     forecast_w = models.DecimalField(
-        max_digits=7, decimal_places=5, blank=True)
+        max_digits=7, decimal_places=5, null=True)
     forecast_d = models.DecimalField(
-        max_digits=7, decimal_places=5, blank=True)
+        max_digits=7, decimal_places=5, null=True)
     forecast_l = models.DecimalField(
-        max_digits=7, decimal_places=5, blank=True)
+        max_digits=7, decimal_places=5, null=True)
 
     class Meta:
-        ordering = ('-date',)
+        ordering = ('-datetime',)
 
     def __str__(self):
-        return f'{self.id_match} - {self.h_title} {self.goals_h} : {self.a_title} {self.goals_a}'
+        return f'{self.id} - {self.h_title} {self.goals_h} : {self.a_title} {self.goals_a}'
 
 
-class Situation(models.Model):
-    id_situation = models.IntegerField(primary_key=True)
+class SituationMatch(models.Model):
+    id = models.IntegerField(primary_key=True)
     minute = models.IntegerField()
     result = models.CharField(max_length=16)
     x = models.DecimalField(max_digits=7, decimal_places=5)
     y = models.DecimalField(max_digits=7, decimal_places=5)
-    xG = models.DecimalField(max_digits=7, decimal_places=5)
+    xg = models.DecimalField(max_digits=7, decimal_places=5)
     player = models.CharField(max_length=60)
     h_a = models.CharField(max_length=2)
     player_id = models.IntegerField()
     situation = models.CharField(max_length=16)
     season = models.IntegerField()
-    shotType = models.CharField(max_length=30)
+    shottype = models.CharField(max_length=30)
     match_id = models.ForeignKey(
-        MatchResult, on_delete=models.CASCADE, related_name='match_situation')
+        MatchResult, on_delete=models.CASCADE, related_name='situation_result')
     h_team = models.CharField(max_length=60)
     a_team = models.CharField(max_length=60)
     h_goals = models.IntegerField()
     a_goals = models.IntegerField()
     date = models.DateTimeField()
-    player_assisted = models.CharField(max_length=60)
-    last_action = models.CharField(max_length=60)
+    player_assisted = models.CharField(max_length=60, null=True)
+    lastaction = models.CharField(max_length=60)
     season = models.IntegerField()
-    league = models.CharField(max_length=20)
+    # league = models.CharField(max_length=20)
 
     class Meta:
         ordering = ('-date',)
@@ -64,12 +64,13 @@ class Situation(models.Model):
     def __str__(self):
         return f'{self.id_situation}, {self.player} - {self.h_team} : {self.a_team}'
 
-class PlayerStatisticInMatch(models.Model):
-    id_player_statistic_in_match = models.IntegerField(primary_key=True)
+
+class Roster(models.Model):
+    id = models.IntegerField(primary_key=True)
     goals = models.IntegerField()
     own_goals = models.IntegerField()
     shots = models.IntegerField()
-    xG = models.DecimalField(max_digits=7, decimal_places=5)
+    xg = models.DecimalField(max_digits=7, decimal_places=5)
     time = models.IntegerField()
     player_id = models.IntegerField()
     team_id = models.IntegerField()
@@ -82,10 +83,10 @@ class PlayerStatisticInMatch(models.Model):
     roster_out = models.IntegerField()
     key_passes = models.IntegerField()
     assists = models.IntegerField()
-    xA = models.DecimalField(max_digits=7, decimal_places=5)
-    xGChain = models.DecimalField(max_digits=7, decimal_places=5)
-    xGBuildup = models.DecimalField(max_digits=7, decimal_places=5)
-    positionOrder = models.IntegerField()
-    match_id = models.ForeignKey(MatchResult, on_delete=models.CASCADE, related_name='player_situation_in_match')
-    season = models.IntegerField()
-    league = models.CharField(max_length=20)
+    xa = models.DecimalField(max_digits=7, decimal_places=5)
+    xgchain = models.DecimalField(max_digits=7, decimal_places=5)
+    xgbuildup = models.DecimalField(max_digits=7, decimal_places=5)
+    positionorder = models.IntegerField()
+    match_id = models.ForeignKey(MatchResult, on_delete=models.CASCADE, related_name='roster_result')
+    # season = models.IntegerField()
+    # league = models.CharField(max_length=20)
--- scrapping/create_database_first_iteration.py
+++ scrapping/create_database_first_iteration.py
@@ -5,19 +5,17 @@ from datetime import datetime
 import psycopg2
 from pandas.io.json._normalize import nested_to_record
 
-
 def scrapper_match_situation(index):
+
     data = _script_to_json(index)
     data_away = data['a']
     data_home = data['h']
     return data_away, data_home
 
-
 def scrapper_match_result():
     data = _script_to_json(1)
     data = nested_to_record(data, sep='_')
     return data
-
 
 def _script_to_json(index):
     """convert scripts to json format"""
@@ -25,13 +23,14 @@ def _script_to_json(index):
     scripts = soup.find_all('script')
     strings = scripts[index].string
 
-    ind_start = strings.index("('") + 2
+    ind_start = strings.index("('")+2
     ind_end = strings.index("')")
 
     json_data = strings[ind_start:ind_end]
     json_data = json_data.encode('utf-8').decode('unicode_escape')
     data = json.loads(json_data)
     return data
+
 
 
 def insert_row(connection, cursor, row, table_name):
@@ -42,16 +41,13 @@ def insert_row(connection, cursor, row, table_name):
     cursor.execute(postgres_insert_query, record_to_insert)
     connection.commit()
 
-
-def create_url(*args, base, sep='/'):
+def create_url(*args, base, sep = '/'):
     return base + '/' + sep.join(args)
-
 
 def create_soup(url):
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'lxml')
     return soup
-
 
 def connect_database():
     connection = psycopg2.connect(user="postgres",
@@ -62,12 +58,10 @@ def connect_database():
     cursor = connection.cursor()
     return connection, cursor
 
-
 def close_database(connection, cursor):
     cursor.close()
     connection.close()
     print("PostgreSQL connection is closed")
-
 
 def situation_to_database():
     data_away, data_home = scrapper_match_situation(index_match_situations)
@@ -75,92 +69,77 @@ def situation_to_database():
     for row_1, row_2 in zip(data_home, data_away):
         insert_row(connection, cursor, row_1, 'home_situationmatch')
         insert_row(connection, cursor, row_2, 'home_situationmatch')
-    for row_1, row_2 in zip(list(rosters_home.values()), list(rosters_home.values())):
+    for row_1, row_2 in zip(list(rosters_home.values()), list(rosters_away.values())):
         row_1['match_id'] = str(id)
         row_2['match_id'] = str(id)
         insert_row(connection, cursor, row_1, 'home_roster')
         insert_row(connection, cursor, row_2, 'home_roster')
 
-
 def match_to_database():
     insert_row(connection, cursor, match, 'home_matchresult')
 
-
 if __name__ == "__main__":
-    # MATCH SITUATION
+    # parametry początkowe
+    start = datetime.now()
     base_url = 'https://understat.com'
+
+    # MATCH SITUATION
     index_match_situations = 1
     index_rosters = 2
 
-    seasons = ['2021']
+    # MATCH RESULT
+    seasons = [str(x) for x in range(2014, 2022)]
     leagues = ['EPL', 'La_Liga', 'Bundesliga', 'Serie_A', 'Ligue_1']
 
-    result = []
-    not_result = []
+    try:
+        connection, cursor = connect_database()
 
-    # wyciąganie ze strony match_id
-    for league in leagues:
-        for season in seasons:
-            url = create_url('league', league, season, base=base_url)
+        # MATCH RESULT
+        # id_result_match = []
+        # not_result = []
+        # for league in leagues:
+        #     for season in seasons:
+        #         url = create_url('league', league, season, base=base_url)
+        #         print(url)
+        #         soup = create_soup(url)
+        #
+        #         data = scrapper_match_result()
+        #         for match in data:
+        #             match['league'] = league
+        #             match['season'] = season
+        #             if match['id'] == '4238':
+        #                 continue
+        #             else:
+        #                 if match['isResult']:
+        #                     id_result_match.append(match['id'])
+        #                 else:
+        #                     not_result.append(match['id'])
+        #                 match_to_database()
+        with open(r'C:\footstats\log\result.txt', 'r') as f_result_last:
+            id_result_match = [x.rstrip() for x in f_result_last.readlines()]  # existing in database result match
+
+        # with open(r'C:\footstats\log\result.txt', 'w') as f_result_now:
+        #     for id in id_result_match:
+        #         f_result_now.write(id + '\n')
+        #
+        # with open(r'C:\footstats\log\not_result.txt', 'w') as f_not_result_now:
+        #     for id in not_result:
+        #         f_not_result_now.write(id + '\n')
+
+        #MATCH SITUATION
+        for id in id_result_match:
+            url = create_url('match', id, base = base_url)
             print(url)
             soup = create_soup(url)
 
-            data = scrapper_match_result()
-            for match in data:
-                if match['id'] == '4238':
-                    continue
-                else:
-                    if match['isResult']:
-                        result.append(match['id'])
-                    else:
-                        not_result.append(match['id'])
+            situation_to_database()
 
-    # porównanie z ostatnią iteracją
-    # with open(r'C:\footstats\log\result.txt', 'r') as f_result_last:
-    #     result_last = [x.rstrip() for x in f_result_last.readlines()]  # existing in database result match
-    with open(r'C:\footstats\log\not_result.txt', 'r') as f_not_result_last:
-        not_result_last = [x.rstrip() for x in f_not_result_last.readlines()]
+    # except (Exception, psycopg2.Error) as error:
+    #     print("Failed to insert record into mobile table", error)
 
-    id_result_match = list(set(result) & set(not_result_last))
-    print(id_result_match)
-
-    with open(r'C:\footstats\log\result.txt', 'w') as f_result_now:
-        for id in result:
-            f_result_now.write(id + '\n')
-
-    with open(r'C:\footstats\log\not_result.txt', 'w') as f_not_result_now:
-        for id in not_result:
-            f_not_result_now.write(id + '\n')
-
-    # update database
-    if id_result_match:
-        try:
-            connection, cursor = connect_database()
-
-            for league in leagues:
-                for season in seasons:
-                    url = create_url('league', league, season, base=base_url)
-                    print(url)
-                    soup = create_soup(url)
-
-                    data = scrapper_match_result()
-                    for match in data:
-                        if match['id'] in id_result_match:
-                            match['league'] = league
-                            match['season'] = season
-                            match_to_database()
-
-            for id in id_result_match:
-                url = create_url('match', id, base=base_url)
-                print(url)
-                soup = create_soup(url)
-
-                situation_to_database()
-
-        except (Exception, psycopg2.Error) as error:
-            print("Failed to insert record into mobile table", error)
-
-        finally:
-            # closing database connection.
-            if connection:
-                close_database(connection, cursor)
+    finally:
+        # closing database connection.
+        if connection:
+            close_database(connection, cursor)
+    stop = datetime.now()
+    print(stop - start)
